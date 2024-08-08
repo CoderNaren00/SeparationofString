0d677d640c3662dbc50f803cb483b5180385ef16
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-08-08 14:58:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparationofString
+{
+    internal class Program
+    {
+        //Separating each character from string
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to be printed :");
+            string name = Console.ReadLine();
+
+            for(int i=0; i<name.Length; i++)
+            {
+                Console.Write(name[i]+ " "+i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
